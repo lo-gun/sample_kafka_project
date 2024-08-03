69e4448c7523a41f9ee7b4068f51ee22ecd41fa3
aakash.kodwani <aakashkodwani@Aakashs-MacBook-Pro.local>
2024-08-03 21:27:37 +0530

    initial setup

---
+++ src/test/java/com/example/sample_kafka_project/SampleKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_kafka_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleKafkaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
